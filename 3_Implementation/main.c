45029187465b415f07a38c30fc3b3b701448db36
deekshitha05 <101087690+deekshitha05@users.noreply.github.com>
2022-04-01 10:48:57 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -9,7 +9,7 @@ int factorial(); // it is header file, conatins all function
 int main() {
   // variable declaration
   int choice; 
-  int n1,n2, ans=0;
+  int num1,num2, answer=0;
   printf("Select the operation you want perform");
   printf("1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n ");
   scanf("%d", &choice);
@@ -19,53 +19,52 @@ int main() {
 
     case 1:
       printf("Enter two number");
-      scanf("%d %d",&n1,&n2);
+      scanf("%d %d",&num1,&num2);
       
-      ans = addition(n1,n2); //calling addition function
+      answer = addition(num1,num2); //calling addition function
       
-      printf("answer=%d",ans);
+      printf("Answer=%d",answer);
       break;
     case 2:
      printf("Enter two number");
-      scanf("%d %d",&n1,&n2);
-      ans=subtraction(n1,n2); //calling subtraction function
-      printf("answer=%d",ans);
+      scanf("%d %d",&num1,&num2);
+      answer=subtraction(num1,num2); //calling subtraction function
+      printf("Answer=%d",answer);
       break;
     case 3:
       printf("Enter two number");
-      scanf("%d %d",&n1,&n2);
-      ans = multiplication(n1,n2); //calling multiply function 
-      printf("answer=%d",ans);
+      scanf("%d %d",&num1,&num2);
+      answer = multiplication(num1,num2); //calling multiply function 
+      printf("Answer=%d",answer);
       break;
     case 4:
       printf("Enter two number");
-      scanf("%d %d",&n1,&n2);
-     
-     ans = division(n1,n2);    //calling division function
-     printf("answer=%d",ans);
+      scanf("%d %d",&num1,&num2);
+     answer = division(num1,num2);    //calling division function
+     printf("Answer=%d",answer);
 
       break;
     case 5: 
         printf("Enter two number");
-        scanf("%d %d",&n1,&n2);
-       ans= modulus(n1,n2);  // calling madules function
-       printf("answer=%d",ans);
+        scanf("%d %d",&num1,&num2);
+       answer= modulus(num1,num2);  // calling madules function
+       printf("Answer=%d",answer);
         break;  
     case 6: 
         printf("\nEnter two numbers to find the power \n");
         printf("number: ");
-        scanf("%d",&n1);
+        scanf("%d",&num1);
  
         printf("power : ");
-        scanf("%d",&n2);      // calling power function
-       ans= power(n1,n2);
-          printf("answer=%d",ans);
+        scanf("%d",&num2);      // calling power function
+       answer= power(num1,num2);
+          printf("Answer=%d",answer);
         break;  
     case 7: 
         printf("\nEnter a number to find factorial : ");
-        scanf("%d",&n1);
-        ans = factorial(n1); //calling  factorial function
-        printf("answer=%d",ans);
+        scanf("%d",&num1);
+        answer = factorial(num1); //calling  factorial function
+        printf("Answer=%d",answer);
         break;  
     // operator doesn't match any case constant
     default:
